664330df7a8704aad325dd7c4922b8dedd4e2714
Psychpsyo <psychpsyo@gmail.com>
2022-01-21 17:34:12 +0100

    fixed last commit

--- server/server.js
+++ server/server.js
@@ -185,7 +185,7 @@ wss.on("connection", function connection(ws) {
 						break;
 					}
 					
-					if (!hasPerms(cubiix, "chat") {
+					if (!hasPerms(cubiix, "chat")) {
 						ws.send("[error]You don't have the permission to send chat messages.");
 						break;
 					}
